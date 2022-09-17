9f7aa617467f980b51b7fc2c4b6415d0a2ec0e8a
Nr1cha <vrm.richards@gmail.com>
2022-09-17 15:56:08 -0600

    adding folders and files for index page

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// const myName = 'Nick Richards';
+// document.querySelector('#name').textContent = myName;
+
+const currentYear = 2022;
+document.querySelector('#year1').textContent = currentYear;
+
+let lastModified = (document.lastModified);
+document.querySelector('#year2').textContent = lastModified;
