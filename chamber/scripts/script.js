518fb04fe33fc9c09ecf7c8ee915953b170565c4
Nr1cha <vrm.richards@gmail.com>
2022-10-08 10:56:37 -0600

    adding initial files for chambers site assignment

---
+++ chamber/scripts/script.js
@@ -0,0 +1,7 @@
+// const currentYear1 new Date.getfullyear
+
+const currentYear = 2022;
+document.querySelector('#year1').textContent = currentYear;
+
+const lastModified = (document.lastModified);
+document.querySelector('#year2').textContent = lastModified;
