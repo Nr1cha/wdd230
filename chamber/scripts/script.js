aec5931cd36a2f166350c41d9914b265909688db
Nr1cha <vrm.richards@gmail.com>
2022-10-08 11:23:23 -0600

    added js date correctly and added placeholder logo

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,7 +1,13 @@
 // const currentYear1 new Date.getfullyear
 
+const now = new Date();
 const currentYear = 2022;
-document.querySelector('#year1').textContent = currentYear;
-
 const lastModified = (document.lastModified);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+// const datefieldUK = document.querySelector("#yearNow"); 
+
+document.querySelector("#yearNow").textContent = fulldateUK;
+document.querySelector('#year1').textContent = currentYear;
 document.querySelector('#year2').textContent = lastModified;
+
+
