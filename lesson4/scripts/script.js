333114426babbd0f6f9bf099f31dd4d2392cad25
Nr1cha <vrm.richards@gmail.com>
2022-10-11 23:26:14 -0600

    adding code for wk 5 assignment

---
+++ lesson4/scripts/script.js
@@ -0,0 +1,11 @@
+// three variables that HOLD references to input, button, and list
+const ref1 = x; 
+const ref2 = y;
+const ref3 = z;
+
+document.getElementsByClassName('input').textContent = x;
+document.querySelector('favchap').textContent = y;
+document.getElementsByClassName('list').textContent = z;
+document.getElementById('button').addEventListener('click', function(){
+    
+})
