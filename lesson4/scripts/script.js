00e85571012dc86e981a1945c9aeee7f6f004c61
Nr1cha <vrm.richards@gmail.com>
2022-10-13 17:52:19 -0600

    added js functionality and linked css

--- lesson4/scripts/script.js
+++ lesson4/scripts/script.js
@@ -1,11 +1,36 @@
 // three variables that HOLD references to input, button, and list
-const ref1 = x; 
-const ref2 = y;
-const ref3 = z;
+// const ref1 = x;
+// const ref2 = y;
+// const ref3 = z;
 
-document.getElementsByClassName('input').textContent = x;
-document.querySelector('favchap').textContent = y;
-document.getElementsByClassName('list').textContent = z;
-document.getElementById('button').addEventListener('click', function(){
-    
-})
+// document.getElementsByClassName('input').textContent = x;
+// document.querySelector('favchap').textContent = y;
+// document.getElementsByClassName('list').textContent = z;
+document.getElementById("button").addEventListener("click", function () {
+    const input = document.getElementById("favchap");
+
+    if (input.value !== "") {
+        // create element
+        const newContent = document.createElement("li");
+        newContent.innerHTML = input.value;
+
+        // create delete button
+        const deleteB = document.createElement("button");
+        // give the button text
+        deleteB.innerHTML = "❌";
+        // appending the button to the new li from line 12
+        newContent.appendChild(deleteB);
+        // append it to an element in the doc
+        document.getElementById("list").appendChild(newContent);
+
+        // eventListener to button to remove item from list
+        deleteB.addEventListener("click", function () {
+            newContent.remove();
+            input.focus();
+            input.value = "";
+        });
+    input.focus();
+    input.value = "";
+
+    }
+});
