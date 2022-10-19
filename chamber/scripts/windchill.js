e2f0962acc8f5769af3efff19ac69c1f06595fad
Nr1cha <vrm.richards@gmail.com>
2022-10-19 09:42:34 -0600

    added base js

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,17 @@
+// Function to get temperature and windspeed, and calculate windchill
+function windChill() {
+    let currentTemp = 80;
+    let windSpeed = 8;
+    let windChill;
+    
+    if (windSpeed > 3 && currentTemp <= 50) {
+        windChill = 35.74 + 0.6215 * currentTemp - 35.75 * windSpeed ** 0.16 + 0.4275 * currentTemp * windSpeed ** 0.16;
+    } else {
+        windChill = "NA";
+    }
+    document.getElementById("temp").innerHTML = currentTemp;
+    document.getElementById("speed").innerHTML = windSpeed;
+    document.getElementById("chill").innerHTML = windChill.toFixed(1);
+}
+
+windChill();
