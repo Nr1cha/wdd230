d17d24e89e9fbc186cf5c2b4b77699e70498f389
Nr1cha <vrm.richards@gmail.com>
2022-10-19 12:31:20 -0600

    adjusting js and html and adding images to serve up different sizes

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -6,12 +6,13 @@ function windChill() {
     
     if (windSpeed > 3 && currentTemp <= 50) {
         windChill = 35.74 + 0.6215 * currentTemp - 35.75 * windSpeed ** 0.16 + 0.4275 * currentTemp * windSpeed ** 0.16;
+        windChill = windChill.toFixed(1)
     } else {
         windChill = "NA";
     }
     document.getElementById("temp").innerHTML = currentTemp;
     document.getElementById("speed").innerHTML = windSpeed;
-    document.getElementById("chill").innerHTML = windChill.toFixed(1);
+    document.getElementById("chill").innerHTML = windChill;
 }
 
 windChill();
