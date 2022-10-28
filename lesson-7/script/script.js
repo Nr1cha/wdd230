de35be885b9690933b03390ca750317414915788
Nr1cha <vrm.richards@gmail.com>
2022-10-27 18:23:40 -0600

    adding blur to css, checking page in js, reformatting html

--- lesson-7/script/script.js
+++ lesson-7/script/script.js
@@ -4,27 +4,33 @@ function preloadImage(img) {
     if (!src) {
         return;
     }
-
     img.src = src;
+    img.onload = () => {img.removeAttribute('data-src')};
 }
 
-// const imgOptions = {
-//     threshhold: o,
-//     rootMargin: "0px 0px 200px 0px"
-// };
-console.log("test-thing");
+const imgOptions = {
+    threshhold: 0,
+    rootMargin: "0px 0px 200px 0px"
+};
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach((entry) => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                imgObserver.unobserve(entry.target);
+            }
+        });
+    }, imgOptions);
+
+    images.forEach(image => {
+        imgObserver.observe(image);
     });
-});
+} else {
+    images.forEach((img) => {
+        preloadImage(img);
+    })
+}
 
-images.forEach(image => {
-    imgObserver.observe(image);
-});
