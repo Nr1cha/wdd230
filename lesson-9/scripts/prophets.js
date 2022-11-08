b9f723fe401b6eeb02acf996d6fc97f63ee46ecc
Nr1cha <vrm.richards@gmail.com>
2022-11-08 16:47:24 -0700

    connecting the json points

--- lesson-9/scripts/prophets.js
+++ lesson-9/scripts/prophets.js
@@ -4,21 +4,14 @@ console.log("whatever");
 
 
 fetch(requestURL)
-  .then(function (response) {
+.then(function (response) {
     return response.json();
-  })
-  .then(function (jsonObject) {
+})
+.then(function (jsonObject) {
+    const prophets = jsonObject['prophets'];
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-  });
-
-// 
-
-  const prophets = jsonObject['prophets'];
-// 
-
-  prophets.forEach(displayProphets);
-
-
+    prophets.forEach(displayProphets);
+});
 
 //   
   function displayProphets(prophet) {
@@ -31,9 +24,10 @@ fetch(requestURL)
     h2.textContent = prophet.name + ' ' + prophet.lastname;
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
-    ____.setAttribute('loading', 'lazy');
+    card.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
