a6928df6ba5adfbc06d345395c768f124cab2fde
Nr1cha <vrm.richards@gmail.com>
2022-11-08 16:09:54 -0700

    adding files for week 9 and removing whitespace

---
+++ lesson-9/scripts/prophets.js
@@ -0,0 +1,44 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+console.log("whatever");
+
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+  });
+
+// 
+
+  const prophets = jsonObject['prophets'];
+// 
+
+  prophets.forEach(displayProphets);
+
+
+
+//   
+  function displayProphets(prophet) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let portrait = document.createElement('img');
+  
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.textContent = prophet.name + ' ' + prophet.lastname;
+
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    ____.setAttribute('src', prophet.imageurl);
+    ____.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    ____.setAttribute('loading', 'lazy');
+  
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(portrait);
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+}
