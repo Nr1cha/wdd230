351dc49e892681bddcf5bb811e74cecbc0a26b30
Nr1cha <vrm.richards@gmail.com>
2022-11-08 22:02:24 -0700

    refactoring fetch request moving js call in html

--- lesson-9/scripts/prophets.js
+++ lesson-9/scripts/prophets.js
@@ -1,19 +1,15 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
-console.log("whatever");
 
-
-fetch(requestURL)
-.then(function (response) {
-    return response.json();
-})
-.then(function (jsonObject) {
+async function getProphetData(){
+    const response = await fetch(requestURL)
+    const jsonObject = await response.json();
     const prophets = jsonObject['prophets'];
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    // temporary checking for valid response and data parsing
+    // console.table(jsonObject);
     prophets.forEach(displayProphets);
-});
-
-//   
+}
+ 
   function displayProphets(prophet) {
     // Create elements to add to the document
     let card = document.createElement('section');
@@ -23,19 +19,19 @@ fetch(requestURL)
     let dob = document.createElement('p');
     let children = document.createElement('p');
 
-    [birthLoc,dob,children].forEach((element) => {
+    // adding classname to elements using a loop
+    [birthLoc, dob, children].forEach((element) => { 
         element.classList.add('caption');
     })
+    // this is a different way of adding a p element and then adding a class name to it. 
     // let p = document.createElement('p');
     // p.classList.add('my-class');
 
-  
     // Change the textContent property of the h2 element to contain the prophet's full name
-    // h2.textContent = prophet.name + ' ' + prophet.lastname;
     h2.textContent = `${prophet.name} ${prophet.lastname}`;
     // adding the elements
-    birthLoc.textContent = `${"Birth Location:"} ${prophet.birthplace}`
-    dob.textContent = `${"Birthdate:"} ${prophet.birthdate}`;
+    birthLoc.textContent = `${"Place of Birth:"} ${prophet.birthplace}`
+    dob.textContent = `${"Date of Birth:"} ${prophet.birthdate}`;
     children.textContent = `${"Number of Children:"} ${prophet.numofchildren}`;
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
@@ -44,14 +40,9 @@ fetch(requestURL)
     portrait.setAttribute('loading', 'lazy');
 
     // Add/append the section(card) with the h2 element
-    card.append(h2,portrait,birthLoc,dob,children);
-    // card.appendChild(h2);
-    // card.appendChild(portrait);
-    // card.appendChild(birthLoc);
-    // card.appendChild(dob);
-    // card.appendChild(children);
+    card.append(h2,birthLoc,dob,children,portrait);
 
-  
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
 }
+getProphetData();
