33c5d8fb6910ac04709fd86a6a15262c46a00b17
Nr1cha <vrm.richards@gmail.com>
2022-11-08 19:06:28 -0700

    adding tliteral

--- lesson-9/scripts/prophets.js
+++ lesson-9/scripts/prophets.js
@@ -40,7 +40,7 @@ fetch(requestURL)
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    portrait.setAttribute('alt', 'Portait of ' + `${prophet.name}` + ` ${prophet.lastname}`);
     portrait.setAttribute('loading', 'lazy');
 
     // Add/append the section(card) with the h2 element
