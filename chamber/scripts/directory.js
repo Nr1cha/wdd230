7b93bef17c98faccd1a49cf984e4a3f316534a6a
Nr1cha <vrm.richards@gmail.com>
2022-11-11 10:19:43 -0700

    adjusting directory json

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,47 +1,51 @@
-// const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+// put URL into variable
+const jsonURL = './data.json';
 const cards = document.querySelector('.cards');
+// console.log('file works');
 
-// async function getProphetData(){
-//     const response = await fetch(requestURL)
-//     const jsonObject = await response.json();
-//     const prophets = jsonObject['prophets'];
-//     // temporary checking for valid response and data parsing
-//     // console.table(jsonObject);
-//     prophets.forEach(displayProphets);
-// }
+
+async function getCompanyData(){
+    const response = await fetch(jsonURL)
+    const jsonObject = await response.json();
+    const companys = jsonObject['companies'];
+    // temporary checking for valid response and data parsing
+    console.table(jsonObject);
+    // console.log('async function works');
+    companys.forEach(displayBusinesses);
+}
 
  
-  function displayProphets(prophet) {
+  function displayBusinesses(company) {
     // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-    let birthLoc = document.createElement('p');
-    let dob = document.createElement('p');
-    let children = document.createElement('p');
+    let picture = document.createElement('img');
+    let phoneNum = document.createElement('p');
+    let address = document.createElement('p');
+    let email = document.createElement('p');
     // adding classname to elements using a loop
-    [birthLoc, dob, children].forEach((element) => { 
+    [phoneNum, address, email].forEach((element) => { 
         element.classList.add('caption');
     })
 
 
 
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    // Change the textContent property of the h2 element to contain the company's full name
     // adding the elements
-    birthLoc.textContent = `${"Place of Birth:"} ${prophet.birthplace}`
-    dob.textContent = `${"Date of Birth:"} ${prophet.birthdate}`;
+    h2.textContent = `${company.name}`;
+    phoneNum.textContent = `${company.phoneNumber}`
+    address.textContent = `${company.address}`;
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', 'Portait of ' + `${prophet.name}` + ` ${prophet.lastname}`);
-    portrait.setAttribute('loading', 'lazy');
+    picture.setAttribute('src', company.image);
+    picture.setAttribute('alt', 'Company name ' + `${company.name}`);
+    picture.setAttribute('loading', 'lazy');
 
     // Add/append the section(card) with the h2 element
-    card.append(h2,dob,birthLoc,portrait);
+    card.append(h2,picture,address,phoneNum);
 
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
 }
 // this function call is to fetch the external data
-// getProphetData();
+getCompanyData();
