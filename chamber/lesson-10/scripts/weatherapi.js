22b7c42d3f2197af293bd9dfd0fa4f1bf65950d3
Nr1cha <vrm.richards@gmail.com>
2022-11-13 21:24:08 -0700

    adding files for lesson 10 with API stuff

---
+++ chamber/lesson-10/scripts/weatherapi.js
@@ -0,0 +1,31 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = '{insert the correct URL here}';
+
+
+// async function
+// async function apiFetch(){
+//     const response = await fetch(url)
+//     const jsonObject = await response.json();
+//     const companys = jsonObject['companies'];
+// }
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
