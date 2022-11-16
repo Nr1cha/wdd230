12315e1c30f814818082067a7f0a8875954e7dd5
Nr1cha <vrm.richards@gmail.com>
2022-11-15 18:34:58 -0700

    adding data to page

--- chamber/lesson-10/scripts/weatherapi.js
+++ chamber/lesson-10/scripts/weatherapi.js
@@ -1,25 +1,14 @@
 // select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
-
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=fairbanks,&units=imperial,&appid=b0b41ac10094817e9cec6c0486ebf081';
-
-
-// async function
-// async function apiFetch(){
-//     const response = await fetch(url)
-//     const jsonObject = await response.json();
-//     const companys = jsonObject['companies'];
-// }
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=fairbanks&units=imperial&appid=b0b41ac10094817e9cec6c0486ebf081';
+const page = document.querySelector('.content');
 
 async function apiFetch() {
     try {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
+        // console.log(data);
+        return data; //added this here.
       } else {
           throw Error(await response.text());
       }
@@ -27,5 +16,21 @@ async function apiFetch() {
         console.log(error);
     }
   }
+
+  function displayWeather(data) {
+    console.log(data);
+    const currentTemp = document.querySelector('#current-temp');
+    const weatherIcon = document.querySelector('#weather-icon');
+    const captionDesc = document.querySelector('figcaption');
+    
+    // adding data from the API to the page
+    currentTemp.textContent = data.main.temp
+    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    captionDesc.textContent = data.weather[0].description
+  }
+  async function init() {
+    const data = await apiFetch();
+    displayWeather(data);
+  }
+  init();
   
-  apiFetch();
