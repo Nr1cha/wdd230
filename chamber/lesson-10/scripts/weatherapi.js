113aaac0c319e0d8ea4fcf0c758b0de7e156b9b7
Nr1cha <vrm.richards@gmail.com>
2022-11-15 18:35:49 -0700

    formatting

--- chamber/lesson-10/scripts/weatherapi.js
+++ chamber/lesson-10/scripts/weatherapi.js
@@ -1,36 +1,36 @@
 // select HTML elements in the document
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=fairbanks&units=imperial&appid=b0b41ac10094817e9cec6c0486ebf081';
-const page = document.querySelector('.content');
+const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=fairbanks&units=imperial&appid=b0b41ac10094817e9cec6c0486ebf081";
+const page = document.querySelector(".content");
 
 async function apiFetch() {
     try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        // console.log(data);
-        return data; //added this here.
-      } else {
-          throw Error(await response.text());
-      }
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            return data; //added this here.
+        } else {
+            throw Error(await response.text());
+        }
     } catch (error) {
         console.log(error);
     }
-  }
+}
 
-  function displayWeather(data) {
+function displayWeather(data) {
     console.log(data);
-    const currentTemp = document.querySelector('#current-temp');
-    const weatherIcon = document.querySelector('#weather-icon');
-    const captionDesc = document.querySelector('figcaption');
-    
+    const currentTemp = document.querySelector("#current-temp");
+    const weatherIcon = document.querySelector("#weather-icon");
+    const captionDesc = document.querySelector("figcaption");
+
     // adding data from the API to the page
-    currentTemp.textContent = data.main.temp
-    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-    captionDesc.textContent = data.weather[0].description
-  }
-  async function init() {
+    currentTemp.textContent = data.main.temp;
+    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    captionDesc.textContent = data.weather[0].description;
+}
+async function init() {
     const data = await apiFetch();
     displayWeather(data);
-  }
-  init();
-  
+}
+init();
