a0cec55ec673c997fcb01303f2cd1183dae12176
Nr1cha <vrm.richards@gmail.com>
2022-12-14 19:43:58 -0700

    updated header and added nav on other pages and fixed form w3 validation

--- bountiful-foods/scripts/weatherapiv2.js
+++ bountiful-foods/scripts/weatherapiv2.js
@@ -26,7 +26,7 @@ function displayResults(weatherdata) {
     const temp = document.createElement("p");
     const conditionDesc = document.createElement("p");
     let currentDay = document.createElement("p");
-
+    let weatherIcon = document.createElement("img");
 
     // adding data from the API to the new elements
     // temp
@@ -40,30 +40,27 @@ function displayResults(weatherdata) {
     currentDay.textContent = `${formattedDate}`;
     // console.log(formattedDate);
 
-
     // adding day info to the p tag
     // currentDay.textContent = `${dateNow}`; 
-
 
     // humidity
     const humid = weatherdata.humidity.toFixed(0);
     humidity.textContent = `Humidity: ${humid}\u0025`;
 
-
     // condition desc
     const cond = weatherdata.weather[0].description;
     conditionDesc.textContent = `Condition: ${cond}`;
 
+    // picture from api
+    weatherIcon.src = `https://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png`;
 
 
     // adding it to the page
     newDiv.classList.add("weather");
     // newDiv.classList.add("day");
-    newDiv.append(currentDay,temp,humidity,conditionDesc);
+    newDiv.append(currentDay,temp,humidity,conditionDesc,weatherIcon);
     document.querySelector("main").appendChild(newDiv);
-
 }
-
 
 async function init() {
     const data = await apiFetch();
